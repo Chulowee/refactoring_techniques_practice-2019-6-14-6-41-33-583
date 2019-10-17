01b398671e92ab520a7772c226a89a346c151660
KATRINA CHLOE REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESKA@OOCL.COM>
2019-10-17 08:46:53 +0800

    Refactored Constant

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -5,13 +5,12 @@ public class BannerRender {
     private static final String MAC = "MAC";
     private static final String IE = "IE";
     private static final String BANNER = "banner";
-    private final String IE_ON_MAC = "IE on Mac?";
 
     public String renderBanner(String platform, String browser) {
         boolean isPlatformMAC = platform.toUpperCase().contains(MAC);
         boolean isBrowserIE = browser.toUpperCase().contains(IE);
         if (isPlatformMAC && isBrowserIE) {
-            return IE_ON_MAC;
+            return "IE on Mac?";
         }
         return BANNER;
     }
